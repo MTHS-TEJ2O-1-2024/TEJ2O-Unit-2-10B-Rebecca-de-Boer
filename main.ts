cc3ed348d49eea6f0c40eb0db594505f139f5dc5
Rebecca-de-Boer <rebecca.de-boer@mths.ca>
2024-11-07 11:17:59 -0500

    ligh

--- main.ts
+++ main.ts
@@ -1,8 +1,50 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Rebecca
+ * Created on: Nov 2024
+ * This program checks how bright the light is
 */
 
-basic.showString('Hello, World!')
+//variables
+let strip = robotbit.rgb()
+
+//setup
+basic.showIcon(IconNames.Happy)
+basic.clearScreen()
+
+//When it is on
+basic.forever(function () {
+    let lightLevel = input.lightLevel()
+    if (lightLevel <= 51) {
+        strip.clear()
+    }
+    if (lightLevel > 52) {
+        strip.clear()
+        strip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
+        strip.show()
+    }
+    if (lightLevel > 104) {
+        strip.clear()
+        strip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
+        strip.setPixelColor(1, neopixel.colors(NeoPixelColors.Red))
+        strip.show()
+    }
+    if (lightLevel > 156) {
+        strip.clear()
+        strip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
+        strip.setPixelColor(1, neopixel.colors(NeoPixelColors.Red))
+        strip.setPixelColor(2, neopixel.colors(NeoPixelColors.Red))
+        strip.show()
+    }
+    if (lightLevel > 208) {
+        strip.clear()
+        strip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
+        strip.setPixelColor(1, neopixel.colors(NeoPixelColors.Red))
+        strip.setPixelColor(2, neopixel.colors(NeoPixelColors.Red))
+        strip.setPixelColor(3, neopixel.colors(NeoPixelColors.Red))
+        strip.show()
+    }
+    // Add a short delay
+    basic.pause(100)
+})
+
